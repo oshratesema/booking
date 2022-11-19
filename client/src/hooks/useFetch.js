8eba188e0d825704dc4ebbc9e862ef8711574d56
Oshrat Tesema <oshratesema0@gmail.com>
2022-11-19 14:19:23 +0200

    commit

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    const reFetch = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+}
+
+
